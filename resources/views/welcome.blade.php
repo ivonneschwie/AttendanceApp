b70c4ff1aa6450b1f7a19125cf760f69f677eb3a
angelo.ponce0068 <angelo.ponce0068@gmail.com>
2026-02-04 15:13:22 +0000

    try css

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,36 +3,39 @@
 <html>
 <head>
     <title>Laravel Firebase Auth</title>
+    <link rel="stylesheet" href="{{ asset('css/login.css') }}">
 </head>
 <body>
-    @if (session('success'))
-        <div style="color: green;">{{ session('success') }}</div>
-    @endif
+    <div class="form-container">
+        @if (session('success'))
+            <div style="color: green;">{{ session('success') }}</div>
+        @endif
 
-    @if ($errors->any())
-        <div style="color: red;">
-            <ul>
-                @foreach ($errors->all() as $error)
-                    <li>{{ $error }}</li>
-                @endforeach
-            </ul>
-        </div>
-    @endif
+        @if ($errors->any())
+            <div class="error-message">
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
 
-    <h2>Signup</h2>
-    <form action="/signup" method="POST">
-        @csrf
-        <input type="email" name="email" placeholder="Email" required value="{{ old('email') }}">
-        <input type="password" name="password" placeholder="Password" required>
-        <button type="submit">Signup</button>
-    </form>
+        <h2>Signup</h2>
+        <form action="/signup" method="POST">
+            @csrf
+            <input type="email" name="email" placeholder="Email" required value="{{ old('email') }}">
+            <input type="password" name="password" placeholder="Password" required>
+            <button type="submit">Signup</button>
+        </form>
 
-    <h2>Login</h2>
-    <form action="/login" method="POST">
-        @csrf
-        <input type="email" name="email" placeholder="Email" required>
-        <input type="password" name="password" placeholder="Password" required>
-        <button type="submit">Login</button>
-    </form>
+        <h2>Login</h2>
+        <form action="/login" method="POST">
+            @csrf
+            <input type="email" name="email" placeholder="Email" required>
+            <input type="password" name="password" placeholder="Password" required>
+            <button type="submit">Login</button>
+        </form>
+    </div>
 </body>
 </html>
